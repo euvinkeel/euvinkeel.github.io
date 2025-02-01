4f2fec943b0f376dc14369f61455afcbc6578b81
euvinkeel <euvinmkeel@gmail.com>
2025-01-31 18:20:59 -0800

    Quartz sync: Jan 31, 2025, 6:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["git", "frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
